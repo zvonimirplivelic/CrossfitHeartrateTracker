e3cf146aaa6d94e5110632df29ddbc0e5c5abb7b
Zvonimir <zvonimirplivelic91@gmail.com>
2021-01-24 18:53:23 +0100

    working on rv and adding member to list, nothing works

---
+++ app/src/main/java/com/zvonimirplivelic/crossfitheartratetracker/ui/fragment/adapter/GymMemberListAdapter.kt
@@ -0,0 +1,4 @@
+package com.zvonimirplivelic.crossfitheartratetracker.ui.fragment.adapter
+
+class GymMemberListAdapter {
+}
---
+++ app/src/main/java/com/zvonimirplivelic/crossfitheartratetracker/util/GymMemberClickListener.kt
@@ -0,0 +1,8 @@
+package com.zvonimirplivelic.crossfitheartratetracker.util
+
+import android.view.View
+
+
+interface GymMemberClickListener {
+    fun onMemberClicked(v: View)
+}
