3f1537559f4b08d05a4a30c876771bf084d16b4a
Zvonimir <zvonimirplivelic91@gmail.com>
2021-02-08 09:25:00 +0100

    add member implemented, showing list with members, but no populated data

--- app/src/main/java/com/zvonimirplivelic/crossfitheartratetracker/ui/fragment/AddMemberFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/crossfitheartratetracker/ui/fragment/AddMemberFragment.kt
@@ -6,8 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.zvonimirplivelic.crossfitheartratetracker.R
+import com.zvonimirplivelic.crossfitheartratetracker.databinding.FragmentAddMemberBinding
+import com.zvonimirplivelic.crossfitheartratetracker.db.GymDatabase
+import com.zvonimirplivelic.crossfitheartratetracker.ui.viewmodel.AddMemberViewModel
+import com.zvonimirplivelic.crossfitheartratetracker.ui.viewmodel.factory.AddMemberViewModelFactory
 
 class AddMemberFragment : Fragment() {
 
@@ -15,14 +21,27 @@ class AddMemberFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_member, container, false)
+        val binding: FragmentAddMemberBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_add_member, container, false
+        )
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = GymDatabase.getInstance(application).gymMemberDao
+        val viewModelFactory = AddMemberViewModelFactory(dataSource, application)
+        val addMemberViewModel = ViewModelProvider(this, viewModelFactory)
+            .get(AddMemberViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        binding.addMemberViewModel = addMemberViewModel
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.btn_add_member).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.actionAddMemberToMemberList)
         }
     }
 }
